e63d0cbf2361986a7c434c1ab6e9fc29e84f8ba2
Praveen <prageck@gmail.com>
2013-09-24 16:28:39 +0530

    added basic modal

--- home.js
+++ home.js
@@ -1,102 +1,106 @@
 $( document ).ready( function () {
-	var contactsJson = [
-		{
-			"id" : "1",
-			"name" : "praveen",
-			"num" : "9999048676",
-			"city": "New Delhi"
-		},
-		{
-			"id" : "2",
-			"name" : "gaurav",
-			"num" : "9999048676",
-			"city": "Loni"
-		},
-		{
-			"id" : "3",
-			"name" : "vik",
-			"num" : "9999048676",
-			"city": "MIG"
-		},
-		{
-			"id" : "4",
-			"name" : "Rahul",
-			"num" : "9999048676",
-			"city": "LIG"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-		{
-			"id" : "5",
-			"name" : "Piyush",
-			"num" : "9999048676",
-			"city": "Delhi"
-		},
-	];
-
-	$( ".contacts-container" ).html("");
 
 	var contactsHtml = "";
+	function getAllContacts() {
+		var contactsJson = [
+			{
+				"id" : "1",
+				"name" : "praveen",
+				"num" : "9999048676",
+				"city": "New Delhi"
+			},
+			{
+				"id" : "2",
+				"name" : "gaurav",
+				"num" : "9999048676",
+				"city": "Loni"
+			},
+			{
+				"id" : "3",
+				"name" : "vik",
+				"num" : "9999048676",
+				"city": "MIG"
+			},
+			{
+				"id" : "4",
+				"name" : "Rahul",
+				"num" : "9999048676",
+				"city": "LIG"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+			{
+				"id" : "5",
+				"name" : "Piyush",
+				"num" : "9999048676",
+				"city": "Delhi"
+			},
+		];
 
-	// <div class="contact-container">
-	// 			<div class="contact-srnum contact-col">
-	// 				1.
-	// 			</div>
-	// 			<div class="contact-icon contact-col">
-					
-	// 			</div>
-	// 			<div class="contact-name contact-col">
-	// 				Praveen
-	// 			</div>
-	// 			<div class="contact-num contact-col">
-	// 				9999048676
-	// 			</div>
-	// 			<div class="contact-city contact-col">
-	// 				New Delhi
-	// 			</div>
-	// 		</div>
+		return contactsJson;
+	}
 
-	$.each(contactsJson, function(i, val) {
-		// console.log(val)
-		console.log(val)
-		contactsHtml += '<div class="contact-container">' +
-							'<div class="contact-srnum contact-col">1.</div>' +
-							'<div class="contact-icon contact-col"></div>' +
-							'<div class="contact-name contact-col">' + val['name'] + '</div>' +
-							'<div class="contact-num contact-col"> ' + val['num'] + ' </div>' +
-							'<div class="contact-city contact-col">New Delhi</div>' +
-						'</div>'
-		// $("#" + i).append(document.createTextNode(" - " + val));
+	updateContactList();
+
+	function updateContactList() {
+		$( ".contacts-container" ).html("");
+		var allContactsJson = getAllContacts();
+		$.each(allContactsJson, function(i, val) {
+			contactsHtml += '<div class="contact-container">' +
+								'<div class="contact-srnum contact-col">1.</div>' +
+								'<div class="contact-icon contact-col"></div>' +
+								'<div class="contact-name contact-col">' + val['name'] + '</div>' +
+								'<div class="contact-num contact-col"> ' + val['num'] + ' </div>' +
+								'<div class="contact-city contact-col">New Delhi</div>' +
+							'</div>'
+		});
+		$( ".contacts-container" ).html( contactsHtml );
+
+		$(".contact-container").unbind("click").click(function() {
+			contactClick();
+		});
+	}
+
+	$(".create-new").click(function() {
+		contactClick();
 	});
-console.log(contactsHtml)
-	$( ".contacts-container" ).html( contactsHtml );
+
+	$(".modal-cancel").click(function() {
+		$(".modal-backdrop").fadeOut(300);
+		$(".contact-modal").fadeOut(300);
+	});
+
+	function contactClick() {
+		$(".modal-backdrop").fadeIn(300);
+		$(".contact-modal").fadeIn(300);
+	}
 
 } );
